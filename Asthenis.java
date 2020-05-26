21c9848f32ac6a2efd57d9b9e010699c5b76ee2a
Stagent <65675304+Stagent@users.noreply.github.com>
2020-05-26 16:17:03 +0300

    1st version of classes

---
+++ Asthenis.java
@@ -0,0 +1,74 @@
+package module_info;
+public class Asthenis {
+    
+    private String username;
+    private String password;
+    private int amka;
+    private String email;
+    private String name;
+    private String surname;
+    private String path_photo;
+    
+    public Asthenis(String username, String password, int amka, String email, String name, String surname, String path_photo)
+    
+    {
+        
+        this.username = username;
+        
+        this.password = password;
+        
+        this.amka = amka;
+        
+        this.email = email;
+        
+        this.name = name;
+        
+        this.surname =surname;
+        
+        this.path_photo = path_photo;
+        
+    }
+
+    public String get_username()
+    {
+    	return username;   
+    }
+
+    public String get_password()
+    {
+    
+    	return password;   
+    }
+
+    public int get_amka()
+    {
+    
+    	return amka;   
+    }
+
+    public String get_name()        
+    {
+    
+    	return name;   
+    }
+
+
+    public String get_email()
+    {
+
+    	return email;
+    }
+
+    public String get_surname()
+    {
+  
+    	return surname;   
+    }
+
+    public String get_path_photo()    
+    {
+    
+    	return path_photo;   
+    }
+}
+
---
+++ Dieuthintis.java
@@ -0,0 +1,30 @@
+package module_info;
+public class Dieuthintis {
+    
+    private String username;
+    private String password;
+    
+    public Dieuthintis(String username, String password)
+            
+    {
+    	
+        this.username = username;
+        
+        this.password = password;
+        
+    }
+    
+    String get_username()
+            
+    {
+        
+     return username;   
+    }
+    
+    String get_password()
+            
+    {
+     return password;   
+    }
+    }
+    
