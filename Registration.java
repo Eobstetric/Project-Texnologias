157f53f69b4d36efb899e0004968a4b97ff27f96
Maria-g07 <62716814+Maria-g07@users.noreply.github.com>
2020-05-26 16:20:40 +0300

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,16 @@
+public class Registration {
+    
+    
+    private int kodikos;
+    private String username;
+    private String password;
+    private int amka;
+    private String email;
+    private String name;
+    private String surname;
+    private String path_photo;
+    private String state;
+    
+    
+    
+}
